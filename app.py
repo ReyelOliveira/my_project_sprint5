384f419f761a0a8af6191979a78565259bb7b048
Reyel <reyeloliveira0@gmail.com>
2026-04-30 18:47:07 -0300

    Atualização do app.py

--- app.py
+++ app.py
@@ -2,13 +2,28 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.header('Projeto 5')
+
 car_data = pd.read_csv('vehicles.csv')  # lendo os dados
+
+if st.checkbox('Mostrar os 10 Modelos Mais Anunciados'):
+    model_counts = car_data['model'].value_counts().head(10)
+
+    fig = px.bar(
+        x=model_counts.index,
+        y=model_counts.values,
+        labels={'x': 'Modelo', 'y': 'Quantidade'},
+        title='Top 10 Modelos Mais Anunciados'
+    )
+
+    fig.update_layout(xaxis_tickangle=-45)
+
+    st.plotly_chart(fig, use_container_width=True)
+
 # criar uma caixa de seleção do histograma
 build_histogram = st.checkbox('Criar histograma')
 # criar uma caixa de seleção do gráfico
 build_scatterplot = st.checkbox('Criar gráfico de dispersão')
-
-st.header('Projeto 5')
 
 if build_histogram:  # se o botão for clicado
     # escrever uma mensagem
@@ -24,7 +39,7 @@ if build_histogram:  # se o botão for clicado
 if build_scatterplot:  # se o botão for clicado
     # escrever uma mensagem
     st.write(
-        'Criando um gráfico para o conjunto de dados de anúncios de vendas de carros')
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um histograma
     fig = px.scatter(car_data, x="odometer", y="price")
